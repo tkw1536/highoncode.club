eaea91bf2537eb694a138897d5651f82ba4e0c2c
Tom Wiesing <tkw01536@gmail.com>
2021-08-15 01:45:42 -0400

    bugfix: Remove legal being loaded twice

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,13 @@ import * as React from 'react';
 import '../styles/latex.css'
 import '../styles/global.css'
 import Script from "next/script";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return <>
-    <Script async strategy="beforeInteractive" src="https://inform.everyone.wtf/legal.min.js?float" />
+    <Head>
+      <script async src="https://inform.everyone.wtf/legal.min.js?float" />
+    </Head>
     <Component {...pageProps} />
   </>
 }
